f7ae7663d9d6ce40d400d9dcc506039b4d09a951
aditiyars <aditiyaramadhansaputra08@gmail.com>
2023-10-11 18:53:32 +0700

    add excerciseTwo

--- src/App.java
+++ src/App.java
@@ -1,45 +1,26 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         // struktur data
 
-        // arraylist
-        // hashset
-        // hashmap
+        // Excercise ONE
+        System.out.println("Excercise ONE\n");
 
-        // List adalah interface
-        // arraylist mengimplentasikan list
-
-        List<Integer> numbers = new ArrayList();
-        
-        // find pair given sum on the list
-        // 3, 4, 5, 9, 1, 2, 0
+        List<Integer> numbers = Arrays.asList(3, 4, 5, 9, 1, 2, 0);
         int sum = 10;
-        
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(9);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(0);
-
         Excercise.findSumNumber(numbers, sum);
+        
         Excercise.soalI(numbers);
 
-        List<String> trainee = new ArrayList<>();
-        trainee.add("Anas");
-        trainee.add("SheiLA");
-        trainee.add("SULAS");
-        trainee.add("Ficri");
-        trainee.add("adiT");
-        trainee.add("dARIAN");
-        trainee.add("ReDhA");
-
-
+        List<String> trainee = Arrays.asList("Anas", "SheiLA", "SULAS", "Ficri", "adiT", "dARIAN", "ReDha");
+        
         Excercise.soalII(trainee);
 
         String[] vowel = {"a", "i", "u", "e", "o"};
@@ -51,5 +32,32 @@ public class App {
         String s = "Hi, my name is Adit";
         Excercise.soalV(s);
 
+        // Excercise Two
+        System.out.println("\nExcercise TWO\n");
+
+        // soal 6
+        List<String> strings = Arrays.asList("a", "b", "a", "c", "b");
+        ExcerciseTwo.soalVI(strings);
+        
+        // soal 7
+        String[] seven = {"man", "moon", "good", "night"};
+        ExcerciseTwo.soalVII(seven);
+
+        // soal 8
+        Map <String, String> map = new HashMap<>();
+        map.put("a", "candy");
+        map.put("b", "dirt");
+        map.put("c", "meh");
+        ExcerciseTwo.soalVIII(map);
+
+        // Soal 9
+        Map<String, String> mapTwo = new HashMap<>();
+        mapTwo.put("a", "aaa");
+        mapTwo.put("b", "bbb");
+        mapTwo.put("c", "ccc");
+        ExcerciseTwo.soalIX(mapTwo);
+        
+        // Soal 10
+        ExcerciseTwo.soalX(3, 1, 9);
     }
 }
